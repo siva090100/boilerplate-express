0be7a0343e0aec8e8c0e36accc805dc180be5975
Siva S <52857258+siva090100@users.noreply.github.com>
2022-09-20 12:57:54 +0000

    handlers included

--- myApp.js
+++ myApp.js
@@ -1,11 +1,18 @@
 let express = require('express');
 let app = express();
+app.use("/public",express.static(__dirname+"/public"));
 
 app.get('/',
 function(req,res){
-    res.send("Hello Express");
+    res.sendFile(__dirname+'/views/index.html');
 })
-console.log("Hello World")
+
+app.get('/json',
+function(req,res)
+{
+  res.json({"message": "Hello json"});
+});
+console.log("Hello World");
 
 
 
