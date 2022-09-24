9d81e8a76795030c0a43d66a61a67cdeed7f2dd7
Siva S <siva090100@gmail.com>
2022-09-24 17:46:33 +0530

    middleware

--- myApp.js
+++ myApp.js
@@ -25,6 +25,16 @@ function(req,res)
 
     res.json({"message":response})
 });
+
+app.get('/now',function(req,res,next){
+
+    req.time = new Date().toString();
+    next();
+},
+function(req,res,next)
+{
+    res.json({"time":req.time});
+});
 console.log("Hello World");
 
 
